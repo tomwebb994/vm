4dba0bd9459641b862f556e1a21c3d523e296f44
tomwebb994 <tomwebb994@proton.me>
2025-09-09 05:25:17 +0000

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,4 +1,16 @@
 @echo off
-echo Alive 👾...
-:recheck
-tasklist | find /i "provisioner.exe" >Nul && (ping 127.0.0.1 >Nul & goto recheck)  || echo "Dead 💀..."
+setlocal enabledelayedexpansion
+
+:loop
+set /a countdown=10  REM Set countdown time in seconds
+
+:countdown
+if !countdown! gtr 0 (
+    echo Countdown: !countdown! seconds remaining...
+    timeout /t 1 > nul  REM Wait for 1 second
+    set /a countdown-=1  REM Decrease countdown by 1
+    goto countdown
+)
+
+echo Time's up! Restarting the loop...
+goto loop
