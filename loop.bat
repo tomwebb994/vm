a6c710e00c2c10436d648659f0476f574fd58ef4
tomwebb994 <tomwebb994@proton.me>
2025-09-09 05:36:35 +0000

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -3,16 +3,12 @@ setlocal
 
 set "condition=true"  REM Initialize the condition variable
 
-:loop
+:recheck
 if "%condition%"=="true" (
     echo The loop is running...
-    timeout /t 999
-
-    REM Here you can add logic to change the condition
-    set /p "userInput=Do you want to continue the loop? (yes/no): "
-    if /i "%userInput%"=="no" (
-        set "condition=false"  REM Change condition to false to exit the loop
-    )
+    timeout /t 9999
+    set "condition=false"  REM Change condition to false to exit the loop
 )
-
+goto recheck
 echo The loop has ended.
+
