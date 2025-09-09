8c8154744a7b9de1a20eb170c7399e225fc9f053
tomwebb994 <tomwebb994@proton.me>
2025-09-09 05:27:35 +0000

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -6,11 +6,12 @@ set /a countdown=10  REM Set countdown time in seconds
 
 :countdown
 if !countdown! gtr 0 (
-    echo Countdown: !countdown! seconds remaining...
-    timeout /t 1 > nul  REM Wait for 1 second
-    set /a countdown-=1  REM Decrease countdown by 1
+    echo Countdown: 
+    timeout /t 1  
+    set /a countdown-=1  
     goto countdown
 )
 
 echo Time's up! Restarting the loop...
 goto loop
+
